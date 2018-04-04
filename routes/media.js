499f1529d7690152eb894555f82e53762be1e1fb
Eric Cobb <ericcobb@Erics-MacBook-Air.local>
2018-04-03 19:24:56 -0700

    updated routes, env files

--- routes/media.js
+++ routes/media.js
@@ -1,6 +1,18 @@
 'use strict';
 
+const fs = require('fs');
+
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
 const express = require('express');
+
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/'});
+
+const path = require('path');
 
 const Media = require('../model/media.js');
 
