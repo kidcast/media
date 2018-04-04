53cac2ec2ace545ac582bc77b16c9d172fc041a7
Brandon Buchholz <blue.buchholz@gmail.com>
2018-04-03 21:26:49 -0700

    setting up bcrypt for password hashing, still need to address pre-save and bearer auth

--- models/user.js
+++ models/user.js
@@ -1,11 +1,29 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Media = require('./media.js');
+const jwt = require('jsonwebtoken');
 
 let userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: String,
+  username: { type: String, required: true, unique: true},
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.methods.checkPassword = function(attempt) {
+  return bcrypt.compare(attempt, this.password);
+};
+   
+userSchema.pre('save', function(next) {
+  if (this.isNew) {
+    bcrypt.hash(this.password, 10).then(hash => {
+      this.password = hash;
+      next();
+    }).catch(err => console.error(err));
+  } else {
+    next();
+  };
 });
 const User = mongoose.model('User', userSchema);
 
