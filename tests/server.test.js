11bbb6103282bc1044ae392208a3bec29001a6b4
amgranad <ambergkim@gmail.com>
2018-04-04 12:43:26 -0700

    tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -33,7 +33,7 @@ describe('Media requests', () => {
       .field('title', newMedia.title)
       .attach('media', mediaLocation)
       .end((err, res) => {
-        console.log('media url', res.body.mediaUrl);
+        console.log('res.body', res.body);
         let amazonUrl = process.env.AWS_BUCKET + '.s3.amazonaws.com';
         let isAmazonUrl = res.body.mediaUrl.includes(amazonUrl);
         expect(isAmazonUrl).toBe(true);
